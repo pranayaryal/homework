3c1de504b6ac90b1c3b551687d36e272047dbdb0
pranayaryal <drpranayaryal@gmail.com>
2016-04-20 17:39:24 -0400

    delete acceptance test

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -5,13 +5,4 @@ $I->amOnPage('/');
 $I->see('Welcome');
 
 
-$J = new AcceptanceTester($scenario);
-$J->wantTo('sign in');
-$J->amOnPage('/register');
-$J->fillField('name', 'pranay');
-$J->fillField('email', 'drpranayaryal@gmail.com');
-$J->fillField('password', '08051978');
-$J->fillField('password_confirmation', '08051978');
 
-$J->click('Register');
-$J->see('pranay');
